d483f2cd870e3f99905dd3c8b049bfc68843ff7b
Felix Nagel <info@felixnagel.com>
2014-08-07 13:40:52 +0200

    [BUGFIX] Another attempt to fix TYPO3 dependencies, see #2

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,8 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
-			'typo3' => '6.1.0-6.1.2',
-			'typo3' => '6.0.0-6.0.7',
+			'typo3' => '6.0.0,6.0.1,6.0.2,6.0.3,6.0.4,6.0.5,6.0.7,6.1.0,6.1.1,6.1.2',
 		),
 		'suggests' => array(),
 	),
