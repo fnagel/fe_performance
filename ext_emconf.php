6335201fd2f92692d187fc5d6855ad2a02826c1e
Felix Nagel <info@felixnagel.com>
2014-08-07 13:58:27 +0200

    [FIX] Merge EM depends and conflicts constraints, see #2

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,11 +32,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.0.7-6.2.99',
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
-			'typo3' => '6.0.0,6.0.1,6.0.2,6.0.3,6.0.4,6.0.5,6.0.7,6.1.0,6.1.1,6.1.2',
+			'typo3' => '6.1.0,6.1.1,6.1.2',
 		),
 		'suggests' => array(),
 	),
