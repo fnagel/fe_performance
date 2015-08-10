4d58335852dfcb121d77a55bc417482fb68f4a56
Felix Nagel <info@felixnagel.com>
2015-08-10 16:52:22 +0200

    [TASK] Test in TYPO3 7.4 and update dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
