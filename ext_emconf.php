1af2cf4c48c948ac84d5fa2adfe112a2850a60e6
Felix Nagel <info@felixnagel.com>
2015-10-04 22:16:41 +0200

    Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.4.99',
+			'typo3' => '6.2.0-7.5.99',
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
