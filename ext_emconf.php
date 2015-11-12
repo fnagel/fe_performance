f835964bf276f0dddd075715a396b0f062829ae1
Felix Nagel <info@felixnagel.com>
2015-11-12 08:50:02 +0100

    [TASK] Consider extension TYPO3 CMS 7.6 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.5.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
