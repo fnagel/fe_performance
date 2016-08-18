2d16b9874170d6f5ecb30817277a19d4e5674fca
Felix Nagel <info@felixnagel.com>
2016-08-19 01:29:21 +0200

    [!!!][TASK] Remove TYPO3 6.2 support, PHP 5.5 minimum according to TYPO3 7.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,13 +27,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.5.4-dev',
+	'version' => '0.6.0-dev',
 	'dependencies' => '',
 	'conflicts' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.4.0-7.0.99',
-			'typo3' => '6.2.0-7.6.99',
+			'php' => '5.5.0-7.0.99',
+			'typo3' => '7.0.0-7.6.99',
 		),
 		'conflicts' => array(
 			'scriptmerger' => '',
