a0bf6e00fce88e7848c1622cd27c6963648b5602
Felix Nagel <info@felixnagel.com>
2019-11-11 01:09:10 +0100

    [TASK] Use class constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ $emConfig = unserialize($_EXTCONF);
 if ($emConfig['minifyJavaScript']) {
     // Add hook for minification
     $GLOBALS['TYPO3_CONF_VARS']['FE']['jsCompressHandler'] =
-        'FelixNagel\\FePerformance\\Hook\\JavaScriptCompressHandlerHook->process';
+        \FelixNagel\FePerformance\Hook\JavaScriptCompressHandlerHook::class . '->process';
 
     // Make sure page JS is not minified before
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_div.php']['minifyJavaScript'] = null;
@@ -18,16 +18,16 @@ if ($emConfig['minifyJavaScript']) {
 if ($emConfig['excludeInlineJsFromConcatenation'] || $emConfig['moveInlineJsToFooter']) {
     // Do not merge per page added inline JS
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-        'FelixNagel\\FePerformance\\Hook\\RenderPreProcessHook->process';
+        \FelixNagel\FePerformance\Hook\RenderPreProcessHook::class . '->process';
 }
 
 if ($emConfig['minifyHtml']) {
     // Add FE hooks for minify the HTML output
     if (TYPO3_MODE === 'FE') {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
-            'FelixNagel\FePerformance\Hook\ContentPostProcHook->processUncachedContent';
+            \FelixNagel\FePerformance\Hook\ContentPostProcHook::class . '->processUncachedContent';
 
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] =
-            'FelixNagel\FePerformance\Hook\ContentPostProcHook->processCachedContent';
+            \FelixNagel\FePerformance\Hook\ContentPostProcHook::class . '->processCachedContent';
     }
 }
