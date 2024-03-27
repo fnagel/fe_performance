38e3e1abd478dd469314c5e8e4ab15ff452c4504
Felix Nagel <info@felixnagel.com>
2024-03-27 17:04:12 +0100

    [!!!][TASK] Add TYPO3 13.x and remove TYPO3 11.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,11 +18,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@felixnagel.com',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '2.2.1-dev',
+    'version' => '2.3.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.2.99',
-            'typo3' => '11.5.0-12.4.99',
+            'php' => '8.1.0-8.3.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
             'scriptmerger' => '',
