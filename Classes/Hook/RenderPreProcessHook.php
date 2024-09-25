6d0994ca3a3fde2c99e79509c74a7b7a4e542050
Felix Nagel <info@felixnagel.com>
2024-09-25 11:17:58 +0200

    [TASK] Check compress hook for TYPO3 v13.3

--- Classes/Hook/RenderPreProcessHook.php
+++ Classes/Hook/RenderPreProcessHook.php
@@ -29,11 +29,12 @@ class RenderPreProcessHook
 
         if (count($params['jsFiles'])) {
             foreach ($params['jsFiles'] as $name => $properties) {
-                // Match file pattern for 8.0.0-12.4.8
+                // Match file pattern for 8.0.0-13.3.0
                 // See here (and other places):
                 // https://github.com/TYPO3/typo3/blob/57944c8c5add00f0e8a1a5e1d07f30a8f20a8201/typo3/sysext/frontend/Classes/Page/PageGenerator.php#L875
                 // https://github.com/TYPO3/typo3/blob/8a455d39c0f1ef1d6b96ad4c7714ebf11317c8df/typo3/sysext/core/Classes/Utility/GeneralUtility.php#L2334
                 // https://github.com/TYPO3/typo3/blob/7bd650b7ee1b42c442819529271f05c0691c03af/typo3/sysext/core/Classes/Utility/GeneralUtility.php#L2559
+                // https://github.com/TYPO3/typo3/blob/49e625c0a0912162020db6e5b454d6501bd39d4b/typo3/sysext/core/Classes/Utility/GeneralUtility.php#L2090
                 if ($emConfig['excludeInlineJsFromConcatenation'] && preg_match('/typo3temp\/assets\/js\/[\d|a-z]+\.js/i', $name)) {
                     $params['jsFiles'][$name]['excludeFromConcatenation'] = 1;
                 }
