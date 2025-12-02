e60307089d22affbbff777a770ca771219012002
Felix Nagel <info@felixnagel.com>
2025-12-02 09:21:27 +0100

    [TASK] Consider extension PHP 8.4 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.3.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
+            'php' => '8.1.0-8.4.99',
             'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
