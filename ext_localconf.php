654da2f49a6f9cf9614ffec56faf6c31c35a15b5
Felix Nagel <info@felixnagel.com>
2026-01-17 19:22:24 +0100

    [CLEANUP] Remove now obsolete excludeFromConcatenation configuration support
    Remove related excludeInlineJsFromConcatenation EM setting.6
    
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.0/Breaking-108055-RemovedPageRendererRelatedHooksAndMethods.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.0/Breaking-108055-RemovedFrontendAssetConcatenationAndCompression.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.0/Breaking-107943-FrontendAndBackendHTTPResponseCompressionRemoved.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,3 @@
 <?php
 
-use FelixNagel\FePerformance\Utility\ExtensionConfigurationUtility;
-use FelixNagel\FePerformance\Hook\RenderPreProcessHook;
-
 defined('TYPO3') || die();
-
-$emConfig = ExtensionConfigurationUtility::get();
-
-if ($emConfig['excludeInlineJsFromConcatenation']) {
-    // Move and do not merge per page added inline JS
-    // https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.0/Breaking-108055-RemovedPageRendererRelatedHooksAndMethods.html
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-        RenderPreProcessHook::class . '->process';
-}
-
